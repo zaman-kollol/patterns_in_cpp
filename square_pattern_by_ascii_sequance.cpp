14792f81e85a47ad62219775b3a1e70c52755c24
Ashikur Zaman Kollol <zamankollol12@gmail.com>
2025-07-16 23:30:08 +0600

    Add files via upload

---
+++ square_pattern_by_ascii_sequance.cpp
@@ -0,0 +1,23 @@
+   /*   A B C D
+        E F G H
+        I J K L
+        M N O P */
+
+
+    #include <iostream>
+    using namespace std;
+
+    int main() {
+        int n=4;
+        int num = 0; //intializing the number to start from 0.
+        
+        for(int i=0; i<n; i++){    //inner loop for rows.
+            for(int j=0; j<n; j++){  //inner loop for columns.
+
+                 cout<<(char)(num+65)<<" "; 
+                num++; 
+        }
+            cout<<endl; //for new line after each row.
+        }
+        return 0;
+    }
