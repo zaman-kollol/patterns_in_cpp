61251a07ce31b2c73ca7c3462647b6180d570945
Ashikur Zaman Kollol <zamankollol12@gmail.com>
2025-07-16 20:38:50 +0600

    Add files via upload

---
+++ square_patterns.cpp
@@ -0,0 +1,23 @@
+
+/*    * * * * *
+      * * * * *
+      * * * * *     */ 
+
+#include<iostream>
+using namespace std;
+int main(){ 
+    
+     int n,n1;
+     cin>>n;
+     cin>>n1; 
+    
+     for(int i=0; i<n; i++){
+        for(int j=0; j<n1; j++){
+
+             cout<<"*"<<" ";
+     }
+           cout<<endl; 
+     }
+    
+    return 0;
+}
