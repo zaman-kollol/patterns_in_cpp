14bd5c19c98b73e14ea18b23f11465363e3fa583
Ashikur Zaman Kollol <zamankollol12@gmail.com>
2025-07-16 23:33:04 +0600

    Add files via upload

---
+++ rectangle_pattern.cpp
@@ -0,0 +1,25 @@
+    
+    /*    * * * *
+          * * * *
+          * * * *    */ 
+
+    
+    
+    #include <iostream>
+    using namespace std;
+
+    int main() {  
+        
+        int n,n1;
+        n=3,n1=4;
+        for(int i=0; i<n; i++){  //loop for rows.
+            for(int j=0; j<n1; j++){ //loop for colums.
+
+                cout<<"*"<<" ";
+        }
+            cout<<endl; 
+        }
+        
+        
+        return 0;
+    }
