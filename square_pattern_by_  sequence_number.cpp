0abb8882328050da2c9a09bde8e13ae9f87fffc5
Ashikur Zaman Kollol <zamankollol12@gmail.com>
2025-07-16 23:19:26 +0600

    Update square_pattern_by_  sequence_number.cpp

--- square_pattern_by_  sequence_number.cpp
+++ square_pattern_by_  sequence_number.cpp
@@ -1,5 +1,5 @@
 
-    /*   0 1 2
+    /*  0 1 2
         3 4 5
         6 7 8  */
 
@@ -11,7 +11,7 @@
         int n=3;
         int num = 0; //intializing the number to start from 0.
         
-        for(int i=0; i<n; i++){    //inner loop for rows.
+        for(int i=0; i<n; i++){    //outer loop for rows.
             for(int j=0; j<n; j++){  //inner loop for columns.
 
                 cout<<num<<" "; 
