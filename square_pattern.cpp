5122ab10aa89953f1ea43c0a6877c47d67e230b5
Ashikur Zaman Kollol <zamankollol12@gmail.com>
2025-07-16 22:53:58 +0600

    Update and rename square_patterns.cpp to square_pattern.cpp

--- square_pattern.cpp
+++ square_pattern.cpp
@@ -1,18 +1,16 @@
 
-/*    * * * * *
-      * * * * *
-      * * * * *     */ 
+/*    * * * 
+      * * * 
+      * * *      */ 
 
 #include<iostream>
 using namespace std;
 int main(){ 
     
-     int n,n1;
-     cin>>n;
-     cin>>n1; 
-    
+     int n=3;
+     
      for(int i=0; i<n; i++){
-        for(int j=0; j<n1; j++){
+        for(int j=0; j<n; j++){
 
              cout<<"*"<<" ";
      }
